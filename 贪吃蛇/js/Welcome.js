1482483863a51e710cb07eed36a3a028fa9d6e4a
Cshine <1071762488@qq.com>
2016-09-07 19:33:22 +0800

    first_commit

---
+++ 贪吃蛇/js/Welcome.js
@@ -0,0 +1,16 @@
+var oForm = document.getElementsByTagName('form')[0];
+var oEnter = oForm.Enter;
+var oSelect = oForm.Select;
+var oMode = oForm.Mode;
+var sValue = '正常';
+
+oEnter.onclick = function(){
+	window.location.href = encodeURI('Snake.html?value='+sValue);
+
+}
+oSelect.onclick= function(){
+	oMode.style.display = 'block';
+}
+oMode.onclick = function(){
+	sValue = this.value;
+}
